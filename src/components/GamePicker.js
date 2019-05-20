6614a8989ec145c7cce3bb8a3396589f7302ffe0
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-05-20 15:34:56 -0400

    Removing git comments added by plugin

--- src/components/GamePicker.js
+++ src/components/GamePicker.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react"
 import axios from "axios"
-<<<<<<< HEAD
 import Select from "react-select"
 
 class GamePicker extends Component {
   state = {
     games: [{ value: "all", label: "All" }],
-=======
-
-class GamePicker extends Component {
-  state = {
-    games: [],
->>>>>>> a8994a684f2038572f96c384b3b70ee18f87d3f4
   }
 
   componentDidMount() {
@@ -24,14 +17,10 @@ class GamePicker extends Component {
       const games = res.data.top
       games.map(result =>
         this.setState({
-<<<<<<< HEAD
           games: [
             ...this.state.games,
             { value: result.game.name, label: result.game.name },
           ],
-=======
-          games: [...this.state.games, result.game.name],
->>>>>>> a8994a684f2038572f96c384b3b70ee18f87d3f4
         })
       )
     })
@@ -51,26 +40,8 @@ class GamePicker extends Component {
       if (gameArray.length !== searchLength) return false
 
       if (searchInput === gameArray.join("")) {
-<<<<<<< HEAD
         console.log([...searchMatches, game])
       }
-=======
-        // console.log(game)
-        console.log([...searchMatches, game])
-      }
-
-      // for (let i = 0; i < gameArray.length; i++) {
-      //   let gameArray.join(""), searchArray.join("")])
-      //   if (gameArray[i] === searchArray[i]) {
-      //     console.log(game)
-      //   }
-      // }
-
-      // if (gameArray == searchArray) {
-      //   console.log(game)
-      //   // return game
-      // }
->>>>>>> a8994a684f2038572f96c384b3b70ee18f87d3f4
     })
   }
 
@@ -87,27 +58,10 @@ class GamePicker extends Component {
     return (
       <div>
         Game:{" "}
-<<<<<<< HEAD
         <Select
           options={this.state.games}
           onChange={this.handleGameChange}
           className="game-search"
-=======
-        <select name="game" id="game" onChange={this.handleGameChange}>
-          <option defaultValue value="all">
-            All
-          </option>
-          {this.state.games.map(game => (
-            <option key={game} value={game}>
-              {game}
-            </option>
-          ))}
-        </select>
-        <input
-          type="text"
-          placeholder="Search games"
-          onChange={this.handleSearch}
->>>>>>> a8994a684f2038572f96c384b3b70ee18f87d3f4
         />
       </div>
     )
