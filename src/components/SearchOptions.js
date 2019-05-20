38932d5b89ba9503e69fb9813002f60d723d00cd
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-05-20 16:35:46 -0400

    Adding some basic styling

--- src/components/SearchOptions.js
+++ src/components/SearchOptions.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react"
+import styled from "styled-components"
 
 import GamePicker from "./GamePicker"
 import StreamCount from "./StreamCount"
 
+const SearchOptionsStyled = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  /* width: 700px; */
+  /* min-width: 400px; */
+`
+
 class SearchOptions extends Component {
   render() {
     return (
-      <div>
+      <SearchOptionsStyled>
         <GamePicker onGameChange={this.props.onGameChange} />
         <StreamCount
           limit={this.props.limit}
           onCountChange={this.props.onCountChange}
         />
-      </div>
+      </SearchOptionsStyled>
     )
   }
 }
