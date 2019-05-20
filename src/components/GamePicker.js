65c084fb18676d3e71b092ece2e5b8697717143b
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-05-20 15:48:57 -0400

    Added ability to clear search field

--- src/components/GamePicker.js
+++ src/components/GamePicker.js
@@ -46,9 +46,15 @@ class GamePicker extends Component {
   }
 
   handleGameChange = e => {
-    let val = e.value
+    let val
+    if (e === null) {
+      val = "\x00"
+    } else {
+      val = e.value
+    }
+    // let val = e.value
     console.log(val)
-    if (e.value === "all") {
+    if (val === "all") {
       val = "\x00"
     }
     this.props.onChangeGame(val)
@@ -62,6 +68,7 @@ class GamePicker extends Component {
           options={this.state.games}
           onChange={this.handleGameChange}
           className="game-search"
+          isClearable
         />
       </div>
     )
