cf462ac2c4586145ca311f8f31065745f31842c0
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-05-21 16:54:32 -0400

    Adjusted grid-gap

--- src/components/styles/TopStreamersStyled.js
+++ src/components/styles/TopStreamersStyled.js
@@ -1,9 +1,11 @@
 import styled from "styled-components"
 
 const TopStreamersStyled = styled.div`
-  margin: 5vh 0;
-  display: grid;
-  grid-template-columns: auto;
-  grid-gap: 2vh 0;
+  .results {
+    margin: 5vh 0;
+    display: grid;
+    grid-template-columns: auto;
+    grid-gap: 1vh 0;
+  }
 `
 export default TopStreamersStyled
