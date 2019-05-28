0a974d2f8912adc1bcba100b4ec4dc1012d4df9c
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-05-28 17:05:37 -0400

    removed padding style

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./styles/layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -30,7 +30,6 @@ const Layout = ({ children }) => (
           style={{
             margin: `0 auto`,
             maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
         >
